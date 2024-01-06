7c9848758833e37a682c2c6ce425d1401457fe9f
DaniloPV18 <damipive1809@gmail.com>
2024-01-06 13:42:21 -0500

    DTO ServicioOfrecido

---
+++ DTOs/ServicioOfrecidoCreacionDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace DHotel_Back.DTOs
+{
+    public class ServicioOfrecidoCreacionDTO
+    {
+        public ServicioOfrecidoCreacionDTO()
+        {
+            FechaRegistro = DateTime.Now;
+            EstadoId = 1;
+        }
+        public string Codigo { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public int? AdministradorId { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public int? EstadoId { get; set; }
+    }
+}
