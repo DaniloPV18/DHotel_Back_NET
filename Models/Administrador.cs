9519c92c4e1ab3980230ad20ef7c722dc4700e30
DaniloPV18 <damipive1809@gmail.com>
2024-01-06 14:08:12 -0500

    Modelo Adm actualizado | Profile Automapper : Admin y ServicioOfrecido

--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -5,23 +5,23 @@ namespace DHotel_Back.Models
     public class Administrador
     {
         public int Id { get; set; }
-        public string Cedula { get; set; } = null!;
-        public string Nombres { get; set; } = null!;
-        public string Apellidos { get; set; } = null!;
-        public string Celular { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public int GeneroId { get; set; }
-        public string Pwd { get; set; } = null!;
-        public DateTime FechaNacimiento { get; set; }
-        public int RolId { get; set; }
-        public int AdministradorId { get; set; }
-        public DateTime FechaRegistro { get; set; }
-        public int EstadoId { get; set; }
-        public DateTime FechaModificacion { get; set; }
-        public DateTime UltimaActividad { get; set; }
-        public HashSet<Habitacion> Habitaciones { get; set; } = new HashSet<Habitacion>();
+        public string? Cedula { get; set; } = null!;
+        public string? Nombres { get; set; } = null!;
+        public string? Apellidos { get; set; } = null!;
+        public string? Celular { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public int? GeneroId { get; set; }
+        public string? Pwd { get; set; } = null!;
+        public DateTime? FechaNacimiento { get; set; }
+        public int? RolId { get; set; }
+        public int? AdministradorId { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public int? EstadoId { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public DateTime? UltimaActividad { get; set; }
+        public HashSet<Habitacion>? Habitaciones { get; set; } = new HashSet<Habitacion>();
         //public HashSet<Huesped> Huespedes { get; set; } = new HashSet<Huesped>();
-        public List<Pago> Pagos { get; set; } = new List<Pago>();
-        public HashSet<ServicioOfrecido> ServicioOfrecido { get; set; } = new HashSet<ServicioOfrecido>();
+        public List<Pago>? Pagos { get; set; } = new List<Pago>();
+        public HashSet<ServicioOfrecido>? ServicioOfrecido { get; set; } = new HashSet<ServicioOfrecido>();
     }
 }
---
+++ Utilidades/Profiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DHotel_Back.DTOs;
+using DHotel_Back.Models;
+
+namespace DHotel_Back.Utilidades
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AdministradorCreacionDTO, Administrador>();
+
+            CreateMap<ServicioOfrecidoCreacionDTO, ServicioOfrecido>();
+        }
+    }
+}
