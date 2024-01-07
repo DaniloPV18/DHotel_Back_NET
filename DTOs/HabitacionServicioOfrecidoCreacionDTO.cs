d702e190e613ac2487ec32141e8e1600ad3c3f9a
DaniloPV18 <damipive1809@gmail.com>
2024-01-06 20:37:58 -0500

    DTO HabitacionServicioOfrecido

---
+++ DTOs/HabitacionServicioOfrecidoCreacionDTO.cs
@@ -0,0 +1,13 @@
+﻿using DHotel_Back.Models;
+using System.Text.Json.Serialization;
+
+namespace DHotel_Back.DTOs
+{
+    public class HabitacionServicioOfrecidoCreacionDTO
+    {
+        public int? HabitacionId { get; set; }
+        public int? ServicioOfrecidoId { get; set; }
+        [JsonIgnore]
+        public DateTime? FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
